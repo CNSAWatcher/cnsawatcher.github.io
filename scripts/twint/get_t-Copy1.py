901eb2bc221f9f8c5777ba4fa50ea902d90bef5e
CNSAWatcher <cnsawatcher@gmail.com>
2022-01-15 17:04:56 -0600

    Update get_t-Copy1.py

--- scripts/twint/get_t-Copy1.py
+++ scripts/twint/get_t-Copy1.py
@@ -7,14 +7,14 @@ import os
 PATH_OF_GIT_REPO = Path(os.getcwd()).parent.parent  # make sure .git folder is properly configured
 COMMIT_MESSAGE = 'auto update'
 def git_push():
-    try:
-        repo = Repo(PATH_OF_GIT_REPO)
-        repo.git.add(all=True)
-        repo.index.commit(COMMIT_MESSAGE)
-        origin = repo.remote(name='origin')
-        origin.push()
-    except Exception as e:
-        print(str(e))    
+#     try:
+    repo = Repo(PATH_OF_GIT_REPO)
+    repo.git.add(all=True)
+    repo.index.commit(COMMIT_MESSAGE)
+    origin = repo.remote(name='origin')
+    origin.push()
+#     except Exception as e:
+#         print(str(e))    
 
 
 while True:
